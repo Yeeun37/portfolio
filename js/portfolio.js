4a3faac1d50045c10d369610b5ebbbb5e03b4481
Yeeun37 <106373635+Yeeun37@users.noreply.github.com>
2022-07-27 16:23:18 +0900

    Add files via upload

--- js/portfolio.js
+++ js/portfolio.js
@@ -44,21 +44,7 @@
 		}
 	});
 	
-	//상단 한글자씩 나오는 기능 (타자치는 효과)
-	const typing = "HELLO, \nI'm Ye Eun"
-	console.log(typing);
-	//$("h1").html( typing[4]  );  "요"
-	let i = 0; 
-	let txt = "";
-	function type(){
-		if( i < typing.length ){			
-			txt += typing[i];
-			document.getElementById("typing").innerText = txt;
-			i++;
-			setTimeout( type, 150 );
-		}
-	}
-	type();
+
 	
 	//휴대폰에서는 상단 배경색 보임
 	if( $(window).width()<= 600 ){
@@ -112,6 +98,51 @@
 				}
 		});		
 	}
+	
+	/*타이핑 애니메이션*/
+			var typingBool = false; 
+			var typingIdx=0; 
+			var liIndex = 0;
+			var liLength = $(".typing-txt>ul>li").length;
+
+			// 타이핑될 텍스트를 가져오기 
+			var typingTxt = $(".typing-txt>ul>li").eq(liIndex).text(); 
+				typingTxt = typingTxt.split(""); // 한글자씩 자름. 
+			
+			if(typingBool==false){ // 타이핑이 진행되지 않았다면 
+				typingBool=true; 
+			
+			var tyInt = setInterval(typing,100); // 반복동작 
+		} 
+     
+			function typing(){ 
+			if(typingIdx<typingTxt.length){ // 타이핑될 텍스트 길이만큼 반복 
+			$(".typing").append(typingTxt[typingIdx]); // 한글자씩 이어준다. 
+				typingIdx++; 
+		
+			}else{ //한문장이끝나면
+					//다음문장으로.. 마지막문장이면 다시 첫번째 문장으로 
+			
+			if(liIndex>=liLength-1){
+			liIndex=0;
+			}else{
+				liIndex++; 
+			}
+     
+			//다음문장을 타이핑하기위한 셋팅
+			typingIdx=0;
+			typingBool = false; 
+			typingTxt = $(".typing-txt>ul>li").eq(liIndex).text(); 
+		   
+			//다음문장 타이핑전 1초 쉰다
+			 clearInterval(tyInt);
+			 setTimeout(function(){
+				$(".typing").html('');
+			   tyInt = setInterval(typing,100);
+			 },1000);
+		} 
+	} 
+	
 	
 });//끝
 
